0064d4763f73b073bc2f190dc1fd4aef2151251d
ruchirajkarki <ruchirajkarki@gmail.com>
2024-10-24 08:24:12 +0545

    fix: Fix blank screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import { useUser } from "./hooks/useUser";
 import { router } from "./Router";
 
 export default function App() {
-  useUser();
+  // useUser();
+  console.log("App component rendered");
   return (
     <ThemeProvider>
       <RouterProvider router={router} />
--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,12 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-const basenameProd = "/mock-messenger";
-
-export default defineConfig(({ command }) => {
-  const isProd = command === "build";
-
-  return {
-    base: isProd ? basenameProd : "",
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    define: {
-      global: {
-        basename: isProd ? basenameProd : "",
-      },
-    },
-  };
+  },
 });
